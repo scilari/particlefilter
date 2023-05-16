a262a307853e44c4c570866f214c4c79d28bd339
Ilari Vallivaara <ilari.vallivaara@gmail.com>
2023-05-16 21:35:48 +0100

    Handle duplicate points

--- particlefilter/src/DataUtils.scala
+++ particlefilter/src/DataUtils.scala
@@ -5,25 +5,54 @@ import scala.collection.mutable.ArrayBuffer
 import com.scilari.math.FloatMath
 
 object DataUtils {
-  def angleBetween(p1: Float2, p2: Float2): Float = {
+  def angleBetween(p1: Float2, p2: Float2, fallBack: Float = 0f): Float = {
     val diff = p2 - p1
-    FloatMath.atan2(diff.y, diff.x)
+    if (diff.lengthSq > 0)
+      FloatMath.atan2(diff.y, diff.x)
+    else
+      fallBack
   }
 
-  // Note: the first control contains the translation and rotation from origo to first position
-  def positionsToControls(ps: IndexedSeq[Float2]): (Pose, Seq[Pose]) = {
-    var angleCurr = angleBetween(ps(0), ps(1))
-    val initialPose = Pose(ps(0), angleCurr)
-    val middleControls = ps.sliding(3, 1).map { case Seq(p0, p1, p2) =>
-      val angleNext = angleBetween(p1, p2)
-      val angleDiff = Angle.angleDiff(angleNext, angleCurr)
-      angleCurr = angleNext
-      Pose(p0.distance(p1), 0, angleDiff)
-    }
-    val lastPose = Pose(ps.last.distance(ps(ps.size - 2)), 0f, 0f)
-    val controls = Seq(Pose.zero) ++ middleControls ++ Seq(lastPose)
+  /**
+    * Computes relative poses from a set of 2D points. These can be interpreted
+    * as piecewise controls, where straight translation is followed by a rotation.
+    * Handles duplicate points. Computes also the initial absolute pose, not included
+    * in the controls.
+    *
+    * @param ps 2D points
+    * @return (absolute initial pose, controls)
+    */
+  def positionsToControls(ps: Seq[Float2]): (Pose, Seq[Pose]) = {
+    val paddedPs = ps.head +: ps :+ ps.last
 
+    val initialHeading =
+      ps.tail.find(_ != ps.head).map { p => (p - ps.head).direction }.getOrElse(0f)
+
+    var angleCurr = initialHeading
+
+    val controls = paddedPs
+      .sliding(3, 1)
+      .map { case Seq(p0, p1, p2) =>
+        val d = p0.distance(p1)
+        if (d > 0f) {
+          angleCurr = angleBetween(p0, p1)
+          val angleNext = angleBetween(p1, p2)
+          Pose(d, 0, Angle.angleDiff(angleNext, angleCurr))
+        } else Pose.zero
+      }
+      .toSeq
+
+    val initialPose = Pose(ps.head, initialHeading)
     (initialPose, controls)
+
+  }
+
+  def controlsToPositions(cs: Seq[Pose], initialPose: Pose = Pose.zero): Seq[Float2] = {
+    val pose = initialPose
+    cs.map { c =>
+      pose.move(c)
+      pose.position.copy()
+    }
   }
 
   def pointsToFile(ps: Iterable[Float2], fileName: String): Unit = {
